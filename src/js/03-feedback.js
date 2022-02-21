d2c1e468ba48e909d206a280a664bddddeef6a45
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-02-21 11:07:29 +0200

    Дз готова

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,59 @@
+var throttle = require('lodash.throttle');
 const formRef = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const localStorageValue = load(LOCALSTORAGE_KEY);
+const inputValue = {};
+
+updateInput(localStorageValue);
 
 const setInputValueForm = e => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const inputValue = {}
-    // if(e.type !== "input"){
-    //     return
-    // }
-    const formData = new FormData(formRef)
-formData.forEach((value, key) => console.log({value, key}))
-    
+  const formData = new FormData(formRef);
+  formData.forEach((value, key) => {
+    if (key === 'email') {
+      inputValue.email = value;
+    } else {
+      inputValue.message = value;
+    }
+  });
 
-    inputValue.email = e.currentTarget.value
-    inputValue.message = e.data
-
-    console.log(inputValue)
-  localStorage.setItem("feedback-form-state", )
+  save(inputValue);
 };
 
-formRef.addEventListener('input', setInputValueForm);
+function save(obj) {
+  try {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(obj));
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
 
+function load(key) {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+function updateInput(obj) {
+  if (obj !== undefined) {
+    formRef.elements.email.value = obj.email;
+    formRef.elements.message.value = obj.message;
+  }
+}
+
+const formClear = e => {
+  e.preventDefault();
+
+  formRef.reset();
+  localStorage.clear();
+
+  console.log(inputValue);
+};
+
+formRef.addEventListener('input', throttle(setInputValueForm, 500));
+formRef.addEventListener('submit', formClear);
+// console.log(localStorageValue)
